22af304aad205ec7633017b9009ea7fd84a27bc2
anirudh-44 <anirudhgadekar44@gmail.com>
2024-07-11 22:01:18 +0530

    added staging env

---
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly ILogger<BoardGamesController> _logger;
+
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetBoardGames")]
+        public IEnumerable<BoardGame> Get() {
+            return new[] {
+                new BoardGame() {
+                    Id = 1,
+                    Name = "Axis & Allies",
+                    Year = 1981
+                },
+                new BoardGame() {
+                    Id = 2,
+                    Name = "Citadels",
+                    Year = 2000
+                },
+                new BoardGame() {
+                    Id = 3,
+                    Name = "Terraforming Mars",
+                    Year = 2016
+                }
+            };
+        }
+    }
+}
--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+/*builder.Services.AddSwaggerGen(opts =>
+ opts.ResolveConflictingActions(apiDesc => apiDesc.First())
+ );*/ // This is for resolving the route conflict and telling swagger to use the one which come first.However it is a bad practice
+      // and we should delete the redundant route unless we actually know what we are doing.So for that we have "exclude from project" 
+      // the ErrorController.cs file.
+
 
 var app = builder.Build();
 
@@ -14,12 +20,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseDeveloperExceptionPage();
 }
+
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapGet("/error", () => Results.Problem()); // this is a minimal api 
+app.MapGet("/error/test", () => { throw new Exception("test"); });
 app.MapControllers();
 
 app.Run();
