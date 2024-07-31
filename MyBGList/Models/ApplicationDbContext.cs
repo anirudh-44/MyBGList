1394931b5121621bb40076ca4991c820bd9d2c32
anirudh-44 <anirudhgadekar44@gmail.com>
2024-07-31 22:28:55 +0530

    Added Entity Framework Core

---
+++ MyBGList/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBGList.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TODO: custom codes here
+        }
+
+    }
+}
