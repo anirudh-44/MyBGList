c07d5176a57458669816442a75892a06b2d070a0
anirudh-44 <anirudhgadekar44@gmail.com>
2024-08-04 23:41:54 +0530

    EFCore continued

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyBGList.DTO;
+using MyBGList.Models;
 
 namespace MyBGList.Controllers
 {
--- MyBGList/Models/ApplicationDbContext.cs
+++ MyBGList/Models/ApplicationDbContext.cs
@@ -14,7 +14,37 @@ namespace MyBGList.Models
         {
             base.OnModelCreating(modelBuilder);
 
-            // TODO: custom codes here
+            modelBuilder.Entity<BoardGames_Domains>()
+                .HasKey(i => new { i.BoardGameId, i.DomainId });
+
+            modelBuilder.Entity<BoardGames_Domains>()
+                .HasOne(x => x.BoardGame)
+                .WithMany(y => y.BoardGames_Domains)
+                .HasForeignKey(f => f.BoardGameId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<BoardGames_Domains>()
+                .HasOne(o => o.Domain)
+                .WithMany(m => m.BoardGames_Domains)
+                .HasForeignKey(f => f.DomainId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<BoardGames_Mechanics>()
+                .HasKey(i => new { i.BoardGameId, i.MechanicId });
+
+            modelBuilder.Entity<BoardGames_Mechanics>()
+                .HasOne(x => x.BoardGame)
+                .WithMany(y => y.BoardGames_Mechanics)
+                .HasForeignKey(f => f.BoardGameId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<BoardGames_Mechanics>()
+                .HasOne(o => o.Mechanic)
+                .WithMany(m => m.BoardGames_Mechanics)
+                .HasForeignKey(f => f.MechanicId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
         }
 
     }
